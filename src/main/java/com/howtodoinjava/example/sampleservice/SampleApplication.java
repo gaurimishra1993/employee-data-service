82cd2db8be087a4a59ceb64224a2aea5a7d2fe89
Gauri Mishra <gauri.mishra@libertymutual.com>
2019-08-01 14:57:10 -0400

    Initial commit

---
+++ src/main/java/com/howtodoinjava/example/sampleservice/SampleApplication.java
@@ -0,0 +1,28 @@
+package com.howtodoinjava.example.sampleservice;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+	
+
+	@Bean(name = "asyncExecutor")
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(3);
+		executor.setMaxPoolSize(3);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("AsynchThread-");
+		executor.initialize();
+		return executor;
+	}
+}
